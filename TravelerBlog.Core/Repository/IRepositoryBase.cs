5809baec6128d05d61451b53887e2c73a1446ebb
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-02 16:48:18 +0300

    Changed some methods

--- TravelerBlog.Core/Repository/IRepositoryBase.cs
+++ TravelerBlog.Core/Repository/IRepositoryBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 using TravelerBlog.Core.Entity;
 
 namespace TravelerBlog.Core.Repository
@@ -11,9 +6,9 @@ namespace TravelerBlog.Core.Repository
     public interface IRepositoryBase<T> where T:EntityBase
     {
         Task<T> AddAsync(T entity);
-        Task<T> AddRangeAsync(IEnumerable<T> entities);
+        Task AddRangeAsync(IEnumerable<T> entities);
         Task<T>DeleteAsync(T entity);
-        Task<T> DeleteRangeAsync(IEnumerable<T> entities);
+        Task DeleteRangeAsync(IEnumerable<T> entities);
         Task<T> UpdateAsync(T entity);
 
 
