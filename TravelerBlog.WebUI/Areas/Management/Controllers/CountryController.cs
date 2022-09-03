77a85c14c7945034ed324b9e400099310eb97152
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 09:48:09 +0300

    some controllers and views were created

---
+++ TravelerBlog.WebUI/Areas/Management/Controllers/CountryController.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using TravelerBlog.Application.Repositories;
+using TravelerBlog.Core.Dtos.CountryDto;
+
+namespace TravelerBlog.WebUI.Areas.Management.Controllers
+{
+    public class CountryController : Controller
+    {
+
+        private readonly ICountryRepository _countryRepository;
+        private readonly IValidator<CreateCountryDto> _validator;
+
+        public CountryController(ICountryRepository countryRepository, IValidator<CreateCountryDto> validator)
+        {
+            _countryRepository = countryRepository;
+            _validator = validator;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddCountry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCountry(CreateCountryDto createCountry)
+        {
+            ValidationResult result = await _validator.ValidateAsync(createCountry);
+
+            Country country = new()
+            {
+                CountryName = createCountry.CountryName,
+                Description = createCountry.Description
+            };
+
+            if (!result.IsValid)
+            {
+                result.AddState(this.ModelState);
+                return View(nameof(Index), createCountry);
+            }
+
+            await _countryRepository.AddAsync(country);
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
