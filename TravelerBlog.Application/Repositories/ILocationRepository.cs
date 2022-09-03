54e2d352b1d119de9754a894cbd100b96af915c1
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 23:43:36 +0300

    A new method was added

--- TravelerBlog.Application/Repositories/ILocationRepository.cs
+++ TravelerBlog.Application/Repositories/ILocationRepository.cs
@@ -1,10 +1,11 @@
-﻿using TravelerBlog.Core.Repository;
+﻿using System.Linq.Expressions;
+using TravelerBlog.Core.Repository;
 using TravelerBlog.Domain.Entities;
 
 namespace TravelerBlog.Application.Repositories
 {
     public interface ILocationRepository:IRepositoryBase<Location>
     {
-
+        Task<IEnumerable<Location>> GetAllWithIncludeAndThanInclude(bool isChangeTracking,Expression<Func<Location, bool>>predicate = null);
     }
 }
