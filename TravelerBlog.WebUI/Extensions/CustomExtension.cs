3441d19e3eaf9164b74092dd57299ed7e6ed885c
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 09:30:39 +0300

    Custom extension was created

---
+++ TravelerBlog.WebUI/Extensions/CustomExtension.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using TravelerBlog.Application.Repositories;
+using TravelerBlog.Application.Validations.AdviceValidators;
+using TravelerBlog.Application.Validations.CityValidators;
+using TravelerBlog.Application.Validations.CountryVaidators;
+using TravelerBlog.Core.Dtos.AdviceDtos;
+using TravelerBlog.Core.Dtos.CityDtos;
+using TravelerBlog.Core.Dtos.CountryDto;
+using TravelerBlog.Persistence.Repositories;
+
+namespace TravelerBlog.WebUI.Extensions
+{
+    public static class CustomExtension
+    {
+        public static IServiceCollection AddIocContainer(this IServiceCollection services)
+        {
+            services.AddScoped<IAdviceRepository, AdviceRepository>();
+            services.AddScoped<ICountryRepository, CountryRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFluentValidation (this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining(typeof(CreateAdviceValidator));
+            services.AddScoped<IValidator<CreateAdviceDto>, CreateAdviceValidator>();
+            services.AddScoped<IValidator<CreateCountryDto>, CreateCountryValidator>();
+            services.AddScoped<IValidator<CreateCityDto>, CreateCityValidator>();
+
+
+            return services;
+        }
+
+    }
+}
