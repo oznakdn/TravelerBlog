5cf44d436873853fc38dea895c36ca9fd4441b93
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 12:23:23 +0300

    CountriesListComponent was created

---
+++ TravelerBlog.WebUI/Components/CountryComponents/CountriesListComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelerBlog.Application.Repositories;
+
+namespace TravelerBlog.WebUI.Components.CountryComponents
+{
+    public class CountriesListComponent : ViewComponent
+    {
+        private readonly ICountryRepository countryRepositorcy;
+
+        public CountriesListComponent(ICountryRepository countryRepositorcy)
+        {
+            this.countryRepositorcy = countryRepositorcy;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var coutries =countryRepositorcy.GetAllAsync(false).Result;
+            var result = coutries.ToList();
+            return View(result);
+        }
+    }
+}
