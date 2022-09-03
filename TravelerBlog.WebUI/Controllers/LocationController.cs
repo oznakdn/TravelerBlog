6aa4c35ca538a89e1383604ab110eeae3eebee87
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 23:45:11 +0300

    Location controller and view added

---
+++ TravelerBlog.WebUI/Controllers/LocationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelerBlog.Application.Repositories;
+
+namespace TravelerBlog.WebUI.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly ILocationRepository _locationRepository;
+
+        public LocationController(ILocationRepository locationRepository)
+        {
+            _locationRepository = locationRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult>GetLocationDetail(Guid id)
+        {
+            var location = await _locationRepository.GetAsync(false, x=>x.Id == id,l=>l.LocationPictures,l=>l.Country);
+            return View(location);
+        }
+    }
+}
