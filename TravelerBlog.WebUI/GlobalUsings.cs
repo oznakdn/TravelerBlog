e7559ad15342f4c15ff74ff1ac48be592af58667
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 09:48:38 +0300

    Global using was modified

---
+++ TravelerBlog.WebUI/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using TravelerBlog.Domain.Entities;
+global using TravelerBlog.Persistence.Data;
+global using TravelerBlog.WebUI.Extensions;
+
+namespace TravelerBlog.WebUI
+{
+    public class GlobalUsings
+    {
+    }
+}
