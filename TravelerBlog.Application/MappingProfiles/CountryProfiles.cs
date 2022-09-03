17dfddf4eaa7d726e00a1ac75b198cdef5bb9273
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-03 09:58:30 +0300

    CoutryProfiles were created for auto mapper

---
+++ TravelerBlog.Application/MappingProfiles/CountryProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelerBlog.Core.Dtos.CountryDto;
+using TravelerBlog.Domain.Entities;
+
+namespace TravelerBlog.Application.MappingProfiles
+{
+    public class CountryProfiles:Profile
+    {
+        public CountryProfiles()
+        {
+            CreateMap<CreateCountryDto, Country>().ReverseMap();
+        }
+    }
+}
